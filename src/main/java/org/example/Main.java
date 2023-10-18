979f09dccf65c6dfbb65d10b2a79a811312f31e0
asrytikov <asrytikov@gmail.com>
2023-10-18 15:26:38 +0400

    Lesson14

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,96 @@
-package org.example;public class Main {
+package org.example;
+
+/*
+Д/з Расширим предыдущую задачу.
+Замерить время добавления, поиска и удаления объектов из коллекции в LinkedList, ArrayList, TreeSet, HashSet.
+Сравнить время и сделать вывод о том, в каких условиях какая коллекция работает быстрее.
+ */
+
+import java.util.*;
+
+public class Main {
+
+    public static final int SIZE = 500000;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<MyClass> arrList = new ArrayList<>();
+        List<MyClass> linkList = new LinkedList<>();
+        TreeSet<MyClass> treeSet = new TreeSet<>();
+        HashSet<MyClass> hashSet = new HashSet<>();
+        System.out.println(addElements(arrList));
+        System.out.println(addElements(linkList));
+        System.out.println(addElements(treeSet));
+        System.out.println(addElements(hashSet));
+        /*System.out.println("----------------------");
+        System.out.println(removeElement(arrList));
+        System.out.println(removeElement(linkList));
+        System.out.println(removeElement(treeSet));
+        System.out.println(removeElement(hashSet));*/
+        System.out.println("--------------------------");
+        MyClass myClass = new MyClass((int)(Math.random()*SIZE), new String());
+        System.out.println(findElement(arrList, myClass));
+        System.out.println(findElement(linkList, myClass));
+        System.out.println(findElement(treeSet, myClass));
+        System.out.println(findElement(hashSet, myClass));
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        if (scanner.hasNextLine()){
+            System.out.printf("Вы ввели какую-то дичь!");
+
+        }else{
+            String str = scanner.nextLine();
+            System.out.println(str);
+        }
+
+
+
+    }
+
+    public static long addElements(Collection collection){
+        long result = 0;
+        long startTime = System.currentTimeMillis();
+          generateCollection(collection);
+        result = System.currentTimeMillis() - startTime;
+        return result;
+    }
+
+    public static long removeElement(Collection collection){
+        long startTime = System.currentTimeMillis();
+        while (collection.iterator().hasNext()){
+            collection.remove(collection.iterator().next());
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static Collection generateCollection(Collection collection){
+        for(int i =0; i<SIZE; i++){
+            collection.add(new MyClass(i, new String()));
+        }
+        System.out.println("Size:" + collection.size());
+        return collection;
+    }
+
+    public static long findElement(Collection collection, MyClass myClass){
+        long startTime = System.currentTimeMillis();
+        Iterator<MyClass> iterator = collection.iterator();
+        int i = 0;
+        while (iterator.hasNext()){
+            if (iterator.next().equals(myClass)){
+                System.out.println("Find " + myClass.getId());
+                break;
+            }
+        }
+        /*for (Object item : collection){
+            if (item.equals(myClass)){
+                System.out.println("Find " + myClass.getId());
+                break;
+            }
+        }*/
+        return System.currentTimeMillis() - startTime;
     }
 }
--- src/main/java/org/example/MyClass.java
+++ src/main/java/org/example/MyClass.java
@@ -1,2 +1,42 @@
-package org.example;public class MyClass {
+package org.example;
+
+import java.util.Objects;
+
+public class MyClass implements Comparable<MyClass>{
+    private int id;
+    private String test;
+
+    public MyClass(int id, String test) {
+        this.id = id;
+        this.test = test;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyClass myClass = (MyClass) o;
+        return id == myClass.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(MyClass o) {
+        if (id> o.getId())
+            return 1;
+        else if (id< o.getId()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
